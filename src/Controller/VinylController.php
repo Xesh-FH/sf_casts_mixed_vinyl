011b1aecdb73e6d3319903a9db9f12f476c14e2c
Emmanuel Bourgeois <emmanuelbourgeois@MacBook-Pro-de-Emmanuel.local>
2022-11-30 17:27:24 +0100

    route -> controller -> Response

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
+class VinylController
+{
+    #[Route('/')]
+    public function hompage(): Response
+    {
+        return new Response('Title: PB and Jams');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Style : ' . u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'Tous les Styles';
+        }
+
+        return new Response($title);
+    }
+}
