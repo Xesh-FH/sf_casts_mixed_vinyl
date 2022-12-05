c6198af5e23501511d62f4e7402d8db42cc9f5be
Emmanuel Bourgeois <emmanuelbourgeois@MacBook-Pro-de-Emmanuel.local>
2022-12-05 15:38:14 +0100

    Add - Non-autowirable parameter in a Service's __construct defined in services.yaml

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
@@ -10,7 +11,8 @@ class MixRepository
 {
     public function __construct(
         protected HttpClientInterface $httpClient,
-        protected CacheInterface $cache
+        protected CacheInterface $cache,
+        private bool $isDebug //Paramètre non 'AutoWirable' dont la valeur est configurée dans le services.yaml
     ) {
     }
 
@@ -19,7 +21,7 @@ class MixRepository
         return $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem) {
             // on définit la durée de vie de cet item du cache à 10 secondes
             // si besoin de clear pool de cache spécifique à l'app => 'php/bin console cache:pool:clear cache.app'
-            $cacheItem->expiresAfter(10);
+            $cacheItem->expiresAfter($this->isDebug ? 10 : 100);
             $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
             return $response->toArray();
         });
