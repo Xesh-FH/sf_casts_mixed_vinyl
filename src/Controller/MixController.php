618d35f88e53d2c4ed93d9e0430536bb9406b9b0
Emmanuel Bourgeois <emmanuelbourgeois@MacBook-Pro-de-Emmanuel.local>
2022-12-21 16:54:54 +0100

    Pckg - composer require zenstruck/foundry --dev

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -14,18 +14,6 @@ class MixController extends AbstractController
     #[Route('/mix/new')]
     public function new(EntityManagerInterface $em): Response
     {
-        $genres = ['pop', 'rock', 'metal', 'video games ost'];
-        $mix = new VinylMix();
-        $mix
-            ->setTitle('Do You Remeber... Phil Collins ?!')
-            ->setDescription('A pure mix of Drummers turned singers')
-            ->setGenre($genres[array_rand($genres)])
-            ->setTrackCount(rand(5, 20))
-            ->setVotes(rand(-50, 50));
-
-        $em->persist($mix);
-        $em->flush();
-
         return new Response(sprintf(
             'Mix %d is %d tracks of 80\'s heaven !',
             $mix->getId(),
